a791e6ba590e2a60f800365f4106f15dbbabaaaa
Simeon03 <t-sbozhilov@microsoft.com>
2023-02-14 14:16:23 +0000

    Added comments :)

--- routes/posts.js
+++ routes/posts.js
@@ -7,6 +7,9 @@ const Post = require("../models/post");
 router.get("/", PostsController.Index);
 router.post("/", PostsController.Create);
 router.get("/new", PostsController.New);
+router.get("/:id", PostsController.PostByID);
 router.patch("/:id/like", PostsController.Like);
+router.get("/:id/comment", PostsController.Comment);
+router.post("/:id", PostsController.PostComment);
 
 module.exports = router;
