dd5c33a0df8e8926a6ceb9ceed0ae5fb390e348b
Simeon03 <t-sbozhilov@microsoft.com>
2023-02-14 09:51:14 +0000

    lfdcsjknds

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-/* eslint-disable comma-dangle */
-/* eslint-disable quotes */
 const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
@@ -24,10 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use("/images", express.static(path.join(__dirname, "/public/images")));
 app.use(methodOverride("_method"));
-
-app.use(express.static("public"));
 
 app.use(
   session({
@@ -52,7 +47,7 @@ app.use((req, res, next) => {
 // middleware function to check for logged-in users
 const sessionChecker = (req, res, next) => {
   if (!req.session.user && !req.cookies.user_sid) {
-    res.redirect("/");
+    res.redirect("/sessions/new");
   } else {
     next();
   }
