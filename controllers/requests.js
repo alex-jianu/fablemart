e197888cb7285f8f18f45faf039f4c69d8f3d5cc
Israa <israa.ibrahim01@outlook.com>
2023-02-14 17:19:24 +0000

    requests

--- controllers/requests.js
+++ controllers/requests.js
@@ -23,10 +23,29 @@ const RequestsController = {
         if (!req.session.user) {
             res.redirect("/");
         } else {
-            const sentRequests = await Request.find({requesterID: req.session.user._id});
-            const receivedRequests = await Request.find({requesteeId: req.session.user._id});
+            const sentRequests = await Request.find({requesterID: req.session.user._id, status: "pending"});
+            const receivedRequests = await Request.find({requesteeID: req.session.user._id, status: "pending"});
             res.render("requests/index", {sentRequests: sentRequests, receivedRequests: receivedRequests});
         }
+
+    },
+
+    Confirm: (req, res) => {
+
+    },
+
+    Decline: async (req, res) => {
+        const request = await Request.findById(req.params.id);
+        const newStatus = "declined";
+        await Request.updateOne({_id: request._id}, {status: newStatus} );
+        res.redirect("/requests");
+    },
+
+    Cancel: async (req, res) => {
+        const request = await Request.findById(req.params.id);
+        const newStatus = "cancelled";
+        await Request.updateOne({_id: request._id}, {status: newStatus} );
+        res.redirect("/requests");
 
     }
 }
--- routes/requests.js
+++ routes/requests.js
@@ -6,6 +6,8 @@ const Request = require("../models/request");
 
 router.post("/:username", RequestsController.Send);
 router.get("/", RequestsController.Index);
-
+router.patch("/:id/confirm", RequestsController.Confirm);
+router.patch("/:id/decline", RequestsController.Decline);
+router.patch("/:id/cancel", RequestsController.Cancel);
 
 module.exports = router;
