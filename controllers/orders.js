c1ae64afb940f3d73134f3f1dbf8b3392b2bebe9
Lauren Boyce <lauren.boyce93@gmail.com>
2023-02-22 16:30:12 +0000

    orders done

--- controllers/orders.js
+++ controllers/orders.js
@@ -1,54 +1,62 @@
 /* eslint-disable quotes */
 const Order = require("../models/order");
 const User = require("../models/user");
+const Item = require("../models/item");
 
 const OrdersController = {
+  Send: async (req, res) => {
+    const order = new Order();
+    order.buyerUsername = req.session.user.username;
+    const item = await Item.findOne({_id: req.params.id})
+    order.itemID = req.params.id;
+    order.sellerUsername = item.owner;
+    order.status = "pending";
+    if (order.buyerUsername === order.sellerUsername) {
+      res.redirect(`/items/${req.params.id}`);
+    } else {
+    order.save((err) => {
+      if (err) {
+        throw err;
+      }
+      res.redirect(`/items/${req.params.id}`);
+    });
+  }
+  },
+
   Index: async (req, res) => {
     if (!req.session.user) {
       res.redirect("/");
     } else {
-      res.render("/orders/index", {
-        orders: orders.reverse(),
+      const sentOrders = await Order.find({buyerUsername: req.session.user.username, status:"pending"})
+      const receivedOrders = await Order.find({sellerUsername: req.session.user.username, status:"pending"})
+      res.render("orders/index", {
+        sentOrders, receivedOrders
       });
     }
   },
 
-  // Confirm: async (req, res) => {
-  //   const order = await Order.findById(req.params.id);
-  //   const newStatus = "confirmed";
-  //   const user1 = await User.findOne({ username: order.buyerUsername });
-  //   const user2 = await User.findOne({ username: order.sellerUsername });
-
-  //   let user1Friends = user1.friends || [];
-  //   user1Friends = user1Friends.concat(user2.username);
-
-  //   let user2Friends = user2.friends || [];
-  //   user2Friends = user2Friends.concat(user1.username);
-
-  //   await User.updateOne(
-  //     { username: user1.username },
-  //     { friends: user1Friends }
-  //   );
-  //   await User.updateOne(
-  //     { username: user2.username },
-  //     { friends: user2Friends }
-  //   );
-  //   await Order.updateOne({ _itemID: order._itemID }, { status: newStatus });
-
-  //   res.redirect("/orders");
-  // },
+  Confirm: async (req, res) => {
+    const order = await Order.findOne({ _id: req.params.id});
+    const newStatus = "confirmed";
+    const newOwner = order.buyerUsername;
+    const item = await Item.findOne({_id: order.itemID});
+    await Order.updateMany({itemID: item._id}, {status: "declined"});
+    await Order.updateOne({ _id: order._id }, { status: newStatus });
+    await Item.updateOne({ _id: item._id}, {owner: newOwner});
+    res.redirect("/orders");
+  },
 
   Decline: async (req, res) => {
-    const order = await Order.findById(req.params.itemID);
+    const order = await Order.findOne({ _id: req.params.id});
     const newStatus = "declined";
-    await Order.updateOne({ _itemID: request._itemID }, { status: newStatus });
+    await Order.updateOne({ _id: order._id }, { status: newStatus });
     res.redirect("/orders");
   },
 
   Cancel: async (req, res) => {
-    const order = await Order.findByItemId(req.params.itemID);
+    const order = await Order.findOne({ _id: req.params.id});
     const newStatus = "cancelled";
-    await Order.updateOne({ _itemID: order._itemID }, { status: newStatus });
+    await Order.updateOne({ _id: order._id }, { status: newStatus });
     res.redirect("/orders");
   },
 };
--- routes/orders.js
+++ routes/orders.js
@@ -5,5 +5,9 @@ const OrdersController = require("../controllers/orders");
 const Order = require("../models/order");
 
 router.get("/", OrdersController.Index);
+router.post("/:id", OrdersController.Send)
+router.patch("/:id/confirm", OrdersController.Confirm);
+router.patch("/:id/decline", OrdersController.Decline);
+router.patch("/:id/cancel", OrdersController.Cancel);
 
 module.exports = router;
