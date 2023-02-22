0ed3f091c7e8fb3e66fadd3b97dc0cc13d60246e
Lauren Boyce <lauren.boyce93@gmail.com>
2023-02-22 11:08:24 +0000

    mid orders

--- controllers/orders.js
+++ controllers/orders.js
@@ -14,10 +14,10 @@ const OrdersController = {
   },
 
   // Confirm: async (req, res) => {
-  //   const request = await Request.findById(req.params.id);
+  //   const order = await Order.findById(req.params.id);
   //   const newStatus = "confirmed";
-  //   const user1 = await User.findOne({ username: request.requesterUsername });
-  //   const user2 = await User.findOne({ username: request.requesteeUsername });
+  //   const user1 = await User.findOne({ username: order.buyerUsername });
+  //   const user2 = await User.findOne({ username: order.sellerUsername });
 
   //   let user1Friends = user1.friends || [];
   //   user1Friends = user1Friends.concat(user2.username);
@@ -33,24 +33,24 @@ const OrdersController = {
   //     { username: user2.username },
   //     { friends: user2Friends }
   //   );
-  //   await Request.updateOne({ _id: request._id }, { status: newStatus });
+  //   await Order.updateOne({ _itemID: order._itemID }, { status: newStatus });
 
-  //   res.redirect("/requests");
+  //   res.redirect("/orders");
   // },
 
-  // Decline: async (req, res) => {
-  //   const request = await Request.findById(req.params.id);
-  //   const newStatus = "declined";
-  //   await Request.updateOne({ _id: request._id }, { status: newStatus });
-  //   res.redirect("/requests");
-  // },
+  Decline: async (req, res) => {
+    const order = await Order.findById(req.params.itemID);
+    const newStatus = "declined";
+    await Order.updateOne({ _itemID: request._itemID }, { status: newStatus });
+    res.redirect("/orders");
+  },
 
-  // Cancel: async (req, res) => {
-  //   const request = await Request.findById(req.params.id);
-  //   const newStatus = "cancelled";
-  //   await Request.updateOne({ _id: request._id }, { status: newStatus });
-  //   res.redirect("/requests");
-  // },
+  Cancel: async (req, res) => {
+    const order = await Order.findByItemId(req.params.itemID);
+    const newStatus = "cancelled";
+    await Order.updateOne({ _itemID: order._itemID }, { status: newStatus });
+    res.redirect("/orders");
+  },
 };
 
 module.exports = OrdersController;
