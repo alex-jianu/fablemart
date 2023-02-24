a7a08d4f8809ec76b81939fdd7d2f04b93870e63
alex-jianu <alextj160398@gmail.com>
2023-02-24 17:19:22 +0000

    messages page done

--- routes/messages.js
+++ routes/messages.js
@@ -5,7 +5,6 @@ const MessagesController = require("../controllers/messages");
 
 router.get("/", MessagesController.Index);
 router.get("/:username", MessagesController.MessagesByUsername);
-router.get("/:username/new", MessagesController.New);
 router.patch("/:username", MessagesController.Create);
 
 module.exports = router;
